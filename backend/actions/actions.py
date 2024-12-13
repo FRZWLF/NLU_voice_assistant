fc29f51611f7447ab1a14d8ed935e771a076633b
FRZWLF <ricorichter02@gmail.com>
2024-12-13 19:01:37 +0100

    Second Commit: implemented get location and wiki intent

--- backend/actions/actions.py
+++ backend/actions/actions.py
@@ -4,14 +4,14 @@ import datetime
 import requests
 from rasa_sdk import Action
 from rasa_sdk.events import SlotSet
+import geocoder
+import wikipedia
 
 TIMEZONEDB_API_KEY = "WSBAFOWS1DLC"
 
 class ActionTellTime(Action):
     def name(self) -> str:
         return "action_tell_time"
-
-
 
     def run(self, dispatcher, tracker, domain):
         entities = tracker.latest_message["entities"]
@@ -60,3 +60,72 @@ class ActionTellTime(Action):
             current_time = datetime.datetime.now().strftime("%H:%M")
             dispatcher.utter_message(text=f"Es ist jetzt {current_time}.")
         return [SlotSet("location", None)]
+
+
+class ActionTellLocation(Action):
+    def name(self) -> str:
+        return "action_tell_location"
+
+    def run(self, dispatcher, tracker, domain):
+        loc = geocoder.ip('me')
+        return dispatcher.utter_message(text=f"Du befindest dich in {loc.city}.")
+
+
+class ActionTellWiki(Action):
+    def name(self) -> str:
+        return "action_tell_wiki"
+
+    def run(self, dispatcher, tracker, domain):
+        entities = tracker.latest_message["entities"]
+        print(f"Wiki query: {entities}")
+        query = next((ent["value"] for ent in entities), None)
+        print(f"Wiki query: {query}")
+
+        if not query:
+            dispatcher.utter_message(text="Ich konnte kein Thema erkennen. Bitte gib ein Thema an.")
+            return []
+
+        wikipedia.set_lang("de")
+
+        try:
+            # Versuche eine Zusammenfassung zu holen
+            summary = wikipedia.summary(query, sentences=2, auto_suggest=True)
+            dispatcher.utter_message(text=f"{summary}")
+            dispatcher.utter_message(text=f"Möchtest du mehr über {query} erfahren?")
+            return [SlotSet("wiki_query", query)]
+        except wikipedia.exceptions.DisambiguationError as e:
+            # Liste der möglichen Bedeutungen zurückgeben
+            options = e.options[:5]  # Maximal 5 Vorschläge
+            options_text = "\n".join(options)
+            dispatcher.utter_message(
+                text=f"Deine Anfrage {query} ist mehrdeutig. Meintest du vielleicht eine dieser Optionen?\n{options_text}"
+            )
+        except wikipedia.exceptions.PageError:
+            # Kein Artikel gefunden
+            dispatcher.utter_message(text=f"Ich konnte keinen Artikel zu '{query}' finden.")
+        except Exception as e:
+            # Allgemeiner Fehler
+            dispatcher.utter_message(text="Es gab ein Problem beim Abrufen der Informationen.")
+            print(f"Fehler: {e}")
+
+        return []
+
+
+class ActionTellMoreWiki(Action):
+    def name(self):
+        return "action_tell_more_wiki"
+
+    def run(self, dispatcher, tracker, domain):
+        query = tracker.get_slot("wiki_query")
+        intent = tracker.get_intent_of_latest_message()
+
+        if intent == "affirm":
+            if query:
+                summary = wikipedia.summary(query, sentences=10)
+                dispatcher.utter_message(text=f"{summary}")
+            else:
+                dispatcher.utter_message(text="Ich konnte das Thema nicht abrufen. Bitte starte die Anfrage erneut.")
+        elif intent == "deny":
+            dispatcher.utter_message(response = "utter_end_by_deny")
+
+        return [SlotSet("wiki_query", None)]
